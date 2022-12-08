f5c247b4e83d67657705453ffcc4f34661716f19
Avisionary <aa2464@georgetown.edu>
2022-12-09 05:04:43 +0530

    new model

--- app/app_NLP.py
+++ app/app_NLP.py
@@ -3,9 +3,14 @@
 import streamlit as st
 import pandas as pd
 import joblib 
-import plotly.express as px
+#import plotly.express as px
 
 #from tensorflow.keras.models import load_model
+import numpy as np
+import pickle
+
+from tensorflow.keras.models import Sequential, model_from_json
+from tensorflow.keras.models import load_model
 import numpy as np
 import pickle
 
@@ -14,73 +19,70 @@ import pickle
 # Title
 st.header("Get Movie Recommendations from an Auto Complete Thoughts Prediction App")
 
-
-
 text=st.text_input("What's on your mind?")
 
-# df = px.data.tips()
-# fig = px.box(df, y="total_bill")
-# fig.show()
+
+def Predict_Next_Words(model, tokenizer, text):
+    for i in range(3):
+        sequence = tokenizer.texts_to_sequences([text])[0]
+        sequence = np.array(sequence)
+        
+        preds = loaded_model.predict(sequence)
+        pp=(-preds[0]).argsort()[:10]
+        #pp=pp[0:5]
+        #preds=np.argmax(preds,axis=1)
+#         print(preds)
+        predicted_word = []
+        
+        for key, value in tokenizer.word_index.items():
+               for word in pp:   
+                       if value == word:
+                        predicted_word.append(key)
+                        break
+        
+        #print(predicted_word)
+        return predicted_word
 
 
-# # Input bar 1
-# u_p_total = st.number_input("Enter the number of times a user has bought the product")
 
-# # Input bar 2
-# uxp_reorder_ratio = st.number_input("Enter the estimated probability of how frequently a user bought the product")
-
-# # Input bar 3
-# u_total_orders= st.number_input("Enter the number of orders placed by a user")
-
-# # Input bar 4
-# u_reordered_ratio = st.number_input("Enter how frequently has a user reordered products")
-
-# # Input bar 5
-# p_total=st.number_input("Enter the number of times a product has been purchased")
-
-# # Input bar 6
-# p_reordered_ratio=st.number_input("Enter the estimated probability of porduct being reordered")
-
-# What does the predicted value mean?
-#st.text('1 means Reordered, 0 means Not reordered')
 
 # If button is pressed
 if st.button("Submit"):
 
-#     model = load_model('nextword1.h5')
-#     tokenizer = pickle.load(open('tokenizer1.pkl', 'rb'))
-    
-#     for i in range(3):
-#         sequence = tokenizer.texts_to_sequences([text])[0]
-#         sequence = np.array(sequence)
-        
-#         preds = model.predict(sequence)
-#         pp=(-preds[0]).argsort()[:10]
-#         #pp=pp[0:5]
-#         #preds=np.argmax(preds,axis=1)
-# #         print(preds)
-#         predicted_word = []
-        
-#         for key, value in tokenizer.word_index.items():
-#                for word in pp:   
-#                        if value == word:
-#                         predicted_word.append(key)
-#                         break
+    # Load the model and tokenizer
+    tokenizer = pickle.load(open('../model/tokenizer_avi.pkl', 'rb'))
+    # load json and create model
+    json_file = open('../model/nextword.json', 'r')
+    loaded_model_json = json_file.read()
+    json_file.close()
+    loaded_model = model_from_json(loaded_model_json)
+    # load weights into new model
+    loaded_model.load_weights("../model/nextword_avi.h5")
 
-        predicted_word='adventures,house,the,toy,dog'
-        st.text(f"This product will be {predicted_word}")
+    text= 'adventures with' 
+    text = text.split(" ")
+    text= text[-1]
 
-        text2=st.text_input("Which predicted word would like to go forward with?")
+    text = ''.join(text)
+    predicted_words = Predict_Next_Words(loaded_model, tokenizer, text)
 
-        if text2=='adventures':
-            st.text(f"Movie Recommendation: Frankie Starlight, The quirky story of a young boy's adventures growing up with his stunningly beautiful mother and the two very different men who love her.")
-            st.text(f"Movie Recommendation: दिल से, The clash between love and ideology is portrayed in this love story between a radio executive and a beautiful revolutionary.")
-            st.text(f"Movie Recommendation: Zebrahead, Interracial love story set in Detroit.")
+    st.write(predicted_words)
 
-        elif text2=='dog':
-            st.text(f"Movie Recommendation: The Dog Problem, In the film, Solo is a down-on-his-luck writer who is encouraged by his psychiatrist to get a dog. Solo meets his love interest, who he assumes to be a dog owner when meeting her at a dog play park, but dog problems stand in their way.")
-            st.text(f"Movie Recommendation: Good Boy!, An intergalactic dog pilot from Sirius (the dog star), visits Earth to verify the rumors that dogs have failed to take over the planet.")
-            st.text(f"Movie Recommendation: Hachi: A Dog's Tale, A drama based on the true story of a college professor's bond with the abandoned dog he takes into his home.")
+
+        # predicted_word='adventures,house,the,toy,dog'
+        # st.text(f"This product will be {predicted_word}")
+
+        # text2=st.text_input("Which predicted word would like to go forward with?")
+
+        # if text2=='adventures':
+        #     st.text(f"Movie Recommendation: Frankie Starlight, The quirky story of a young boy's adventures growing up with his stunningly beautiful mother and the two very different men who love her.")
+        #     st.text(f"Movie Recommendation: दिल से, The clash between love and ideology is portrayed in this love story between a radio executive and a beautiful revolutionary.")
+        #     st.text(f"Movie Recommendation: Zebrahead, Interracial love story set in Detroit.")
+
+        # elif text2=='dog':
+        #     st.text(f"Movie Recommendation: The Dog Problem, In the film, Solo is a down-on-his-luck writer who is encouraged by his psychiatrist to get a dog. Solo meets his love interest, who he assumes to be a dog owner when meeting her at a dog play park, but dog problems stand in their way.")
+        #     st.text(f"Movie Recommendation: Good Boy!, An intergalactic dog pilot from Sirius (the dog star), visits Earth to verify the rumors that dogs have failed to take over the planet.")
+        #     st.text(f"Movie Recommendation: Hachi: A Dog's Tale, A drama based on the true story of a college professor's bond with the abandoned dog he takes into his home.")
 
 
 
