a83e72ae8f36554e4a841a3a74c5efe9418c92f1
Avisionary <aa2464@georgetown.edu>
2022-12-09 05:58:51 +0530

    app update

--- app/app_NLP.py
+++ app/app_NLP.py
@@ -1,6 +1,7 @@
 # pip install -r /path/to/requirements.txt
 
 import streamlit as st
+#import SessionState
 import pandas as pd
 import joblib 
 #import plotly.express as px
@@ -44,11 +45,20 @@ def Predict_Next_Words(model, tokenizer, text):
         return predicted_word
 
 
+print_sentence1 = st.empty()
+
+print_sentence2 = st.empty()
+
+#ss = SessionState.get(button1 = False)
+
+ques1 = st.radio(
+
+    "Ready to get prediction?",
+
+    ('No','Yes'))
 
 
-# If button is pressed
-if st.button("Submit"):
-
+if ques1 == "Yes":
     # Load the model and tokenizer
     tokenizer = pickle.load(open('../model/tokenizer_avi.pkl', 'rb'))
     # load json and create model
@@ -59,31 +69,24 @@ if st.button("Submit"):
     # load weights into new model
     loaded_model.load_weights("../model/nextword_avi.h5")
 
-    text= 'adventures with' 
-    text = text.split(" ")
-    text= text[-1]
+    #text= 'adventures with' 
+    text2 = text.split(" ")
+    text2= text2[-1]
 
-    text = ''.join(text)
+    text2 = ''.join(text2)
     predicted_words = Predict_Next_Words(loaded_model, tokenizer, text)
+    predicted_sentence = " ".join(predicted_words)
+    mk_sentnce = f'<p style="font-family:Courier; color:Black; font-size: 15px;">Predicted Sentence:</p><p style="font-family:Courier; color:Blue; font-size: 20px;">{text} "{predicted_sentence}"</p>'
+    print(predicted_words)
+    #st.markdown(f"Predicted Sentence:")
+    st.markdown(mk_sentnce,unsafe_allow_html=True)
+    
 
-    st.write(predicted_words)
+    picked_word=st.text_input("Which predicted word would like to go forward with?")
 
-
-        # predicted_word='adventures,house,the,toy,dog'
-        # st.text(f"This product will be {predicted_word}")
-
-        # text2=st.text_input("Which predicted word would like to go forward with?")
-
-        # if text2=='adventures':
-        #     st.text(f"Movie Recommendation: Frankie Starlight, The quirky story of a young boy's adventures growing up with his stunningly beautiful mother and the two very different men who love her.")
-        #     st.text(f"Movie Recommendation: दिल से, The clash between love and ideology is portrayed in this love story between a radio executive and a beautiful revolutionary.")
-        #     st.text(f"Movie Recommendation: Zebrahead, Interracial love story set in Detroit.")
-
-        # elif text2=='dog':
-        #     st.text(f"Movie Recommendation: The Dog Problem, In the film, Solo is a down-on-his-luck writer who is encouraged by his psychiatrist to get a dog. Solo meets his love interest, who he assumes to be a dog owner when meeting her at a dog play park, but dog problems stand in their way.")
-        #     st.text(f"Movie Recommendation: Good Boy!, An intergalactic dog pilot from Sirius (the dog star), visits Earth to verify the rumors that dogs have failed to take over the planet.")
-        #     st.text(f"Movie Recommendation: Hachi: A Dog's Tale, A drama based on the true story of a college professor's bond with the abandoned dog he takes into his home.")
-
+    if picked_word=='adventures':
+        st.write(f"Movie Recommendation: Frankie Starlight, The quirky story of a young boy's adventures growing up with his stunningly beautiful mother and the two very different men who love her.")
+        
 
 
 
